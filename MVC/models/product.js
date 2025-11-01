9bdf490cbb0b15198d469b2ec1c3e069908d6371
BeingmuqhtadeerM <muhammedmuzawar9@gmail.com>
2025-11-01 12:17:10 +0530

    ejs added and fixed

--- MVC/models/product.js
+++ MVC/models/product.js
@@ -1,17 +1,40 @@
-const products = [];
+const { fileLoader } = require("ejs");
+const fs = require("fs");
+const path = require("path");
+
+const getProductsFormFile = (cb) => {
+  const p = path.join(
+    path.dirname(process.mainModule.filename),
+    "data",
+    "products.json"
+  );
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
 
 // this class defines the shap of the projjects
-
+const products = [];
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    products.push(this);
+    getProductsFormFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+
+    fs.readFile(p, (err, fileContent) => {});
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFormFile(cb);
   }
 };
